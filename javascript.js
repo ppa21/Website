a578c702600d79e2987a74bdd18ed7ae9c093ed7
pruthviraj1998 <pruthviraj1998@hotmail.com>
2019-12-30 17:12:49 -0800

    Website 2.0

---
+++ javascript.js
@@ -0,0 +1,56 @@
+$(".dLink").css("color", "black");
+
+$(".dLink").css({"text-decoration":"none"});
+
+$(".navItems").hover(
+  function() {
+    $(this).css("color", "#808080");
+  }, function() {
+    $(this).css("color", "white");
+  }
+);
+
+$(".navbar-brand").hover(
+  function() {
+    $(this).css("color", "#808080");
+  }, function() {
+    $(this).css("color", "white");
+  }
+);
+
+$("#projects").css({"text-decoration":"none"});
+$("#projects").css("color", "rgb(29, 218, 144)");
+
+$("#contact").css({"text-decoration":"none"});
+$("#contact").css("color", "rgb(29, 218, 144)");
+
+$(".rLink").css({"text-decoration":"none"});
+$(".rLink").css("color", "rgb(29, 218, 144)");
+
+$(".resume").css({"text-decoration":"none"});
+$(".resume").css("color", "white");
+
+$("#linkedin").css({"text-decoration":"none"});
+$("#linkedin").css("color", "white");
+
+$("#git").css({"text-decoration":"none"});
+$("#git").css("color", "white");
+
+$("#name").css("color", "rgb(29, 218, 144)");
+
+$(".footer-text-static").css("color", "white");
+
+$(".footer-text").css("color", "rgb(29, 218, 144)");
+$(".footer-text").css({"text-decoration":"none"});
+
+$(".footer-text-main").css("color", "white");
+$(".footer-text-main").css({"text-decoration":"none"});
+
+$(".footer-text").hover(
+  function() {
+    $(this).css("color", "white");
+  }, function() {
+    $(this).css("color", "rgb(29, 218, 144)");
+  }
+);
+
